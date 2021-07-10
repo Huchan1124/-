e554f94f87f5ccc0590f486d663a8e4e70ed4dc2
Huchan1124 <garlic1124@gmail.com>
2021-07-10 13:59:11 +0800

    完成title modal 新增項目功能的彈出視窗設定

--- web01/modal/title.php
+++ web01/modal/title.php
@@ -1,5 +1,26 @@
 <?php
-
-echo "我是標題圖片的分頁喔";
-
+/*記得引入 base 檔，因為用Ajax方式傳遞沒辦法繼承，同樣也沒辦法透過url抓取變數，
+所以我們直接用輸入文字的方式解決*/
+include_once "../base.php";
 ?>
+
+<h3 class="cent"><?= $adStr['title'];?></h3>
+<hr>
+<!-- 因為要上傳檔案，要傳比較大的檔案，資料量大的建議使用post -->
+<!-- 另外絕對要記得加上 enctype 才能上傳檔案 multipart/form-data(多媒體資料格式/以表單方式傳送)-->
+<form action="../api/upload.php" method="post" enctype="multipart/form-data">
+<table style="margin: 0 auto;">
+    <tr>
+        <td><?= $hStr['title'];?></td>
+        <td><input type="file" name="img" ></td>
+    </tr>
+    <tr>
+        <td>標題區替代文字:</td>
+        <td><input type="text" name="text" ></td>
+    </tr>
+</table>
+<div class="cent">
+    <input type="submit" value="新增" >
+    <input type="reset" value="重置">
+</div>
+</form>
